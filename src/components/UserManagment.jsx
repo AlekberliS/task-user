4a151cff690760d299c3ea47dd82bc48ecdfa22c
AlekberliS <117600954+AlekberliS@users.noreply.github.com>
2025-02-22 11:54:18 +0400

    second commit

--- src/components/UserManagment.jsx
+++ src/components/UserManagment.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState, useContext } from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { ThemeContext } from './ThemeContext';
+import { useEffect, useState, useContext } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { ThemeContext } from "./ThemeContext";
 import UserModal from "./UserModal";
-import Select from 'react-select';
+import Select from "react-select";
 export default function UserManagement() {
   const [users, setUsers] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -12,7 +12,7 @@ export default function UserManagement() {
   // const [searchUser, setSearchUser] = useState('');
   const generateEmployeeNumber = () => {
     const lastDigit = Math.floor(Math.random() * 3) + 1;
-    console.log(Math.floor(Math.random()*3))//0-2
+    console.log(Math.floor(Math.random() * 3)); //0-2
     const firstFourDigits = Math.floor(1000 + Math.random() * 9000);
     return parseInt(`${firstFourDigits}${lastDigit}`);
   };
@@ -22,13 +22,14 @@ export default function UserManagement() {
     if (lastDigit === 1) return `${name} Ərizəsi`;
     if (lastDigit === 2) return `${name} Təqdimatı`;
     if (lastDigit === 3) return `${name} Razılıq Ərizəsi`;
-   
   };
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
         const data = await response.json();
         const updatedUsers = data.map((user) => {
           const employeeNumber = generateEmployeeNumber();
@@ -40,29 +41,22 @@ export default function UserManagement() {
           };
         });
         setUsers(updatedUsers);
-        console.log(updatedUsers)
+        console.log(updatedUsers);
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error("Error fetching users:", error);
       }
     };
 
     fetchUsers();
   }, []);
 
-  const validationSchema = Yup.object({
-    rows: Yup.array().of(
-      Yup.object({
-        id: Yup.string().required('İşçi seçilməlidir'),
-       
-      })
-    ),
-  });
+ 
 
   const formik = useFormik({
     initialValues: {
-      rows: [{ id: '', note: '' }],
+      rows: [{ id: "", note: "" }],
     },
-    validationSchema,
+    // validationSchema,
     onSubmit: (values) => {
       console.log(values);
     },
@@ -70,7 +64,7 @@ export default function UserManagement() {
 
   const handleAddRow = () => {
     formik.setValues({
-      rows: [...formik.values.rows, { id: '', note: '' }],
+      rows: [...formik.values.rows, { id: "", note: "" }],
     });
   };
 
@@ -91,18 +85,27 @@ export default function UserManagement() {
     const updatedRows = formik.values.rows.filter((_, i) => i !== index);
     formik.setValues({ rows: updatedRows });
   };
-  const options = users.map(user => ({
+  const options = users.map((user) => ({
     value: user.id,
-    label: `${user.name} - ${user.employeeNumber}`
+    label: `${user.name} - ${user.employeeNumber}`,
   }));
   return (
-    <div className={`${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'} min-h-screen p-4 flex flex-col items-center`}>
+    <div
+      className={`${
+        theme === "dark" ? "bg-gray-900 text-white" : "bg-white text-black"
+      } min-h-screen p-4 flex flex-col items-center`}
+    >
       <div className="container mx-auto p-4 text-white min-h-screen">
         <div className="header flex justify-between">
-          <h1 className="md:text-3xl font-bold mb-6 text-blue-400">İşçi Məlumatları</h1>
+          <h1 className="md:text-3xl font-bold mb-6 text-blue-400">
+            İşçi Məlumatları
+          </h1>
           <div className="right-sec flex">
-            <button onClick={toggleTheme} className="text-white px-4 py-2 rounded-lg transition duration-300">
-              {theme === 'dark' ? '🌞' : '🌙'}
+            <button
+              onClick={toggleTheme}
+              className="text-white px-4 py-2 rounded-lg transition duration-300"
+            >
+              {theme === "dark" ? "🌞" : "🌙"}
             </button>
             <button
               onClick={handleAddRow}
@@ -113,10 +116,25 @@ export default function UserManagement() {
           </div>
         </div>
 
-        <form onSubmit={formik.handleSubmit} className="employee-data bg-white p-4 rounded-lg shadow-lg">
-          <table className={`w-full rounded-lg text-sm ${theme === 'light' ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
+        <form
+          onSubmit={formik.handleSubmit}
+          className="employee-data bg-white p-4 rounded-lg shadow-lg"
+        >
+          <table
+            className={`w-full rounded-lg text-sm ${
+              theme === "light"
+                ? "bg-gray-800 text-white"
+                : "bg-white text-black"
+            }`}
+          >
             <thead>
-              <tr className={`rounded-lg ${theme === 'light' ? 'text-gray-300 ' : 'text-slate-500 bg-gray-200'}` }>
+              <tr
+                className={`rounded-lg ${
+                  theme === "light"
+                    ? "text-gray-300 "
+                    : "text-slate-500 bg-gray-200"
+                }`}
+              >
                 <th className="p-2">Sıra</th>
                 <th className="p-2">İşçi</th>
                 <th className="p-2">Xüsusi Qeyd</th>
@@ -125,62 +143,60 @@ export default function UserManagement() {
             </thead>
             <tbody>
               {formik.values.rows.map((row, index) => (
-                <tr key={index} className={`${theme === 'light' ? 'hover:bg-gray-700' : 'hover:bg-gray-100'}`}>
-                  <td className="p-2 text-center font-semibold ">{index + 1}</td>
-                  <td className='max-w-44'>
-                  <Select
-  options={options}
-  isSearchable
-  placeholder="İşçilər..."
-  onChange={(selectedOption) => {
-    const selectedUser = users.find(u => u.id === selectedOption.value);
-    const updatedRows = [...formik.values.rows];
-    updatedRows[index] = { 
-      id: selectedUser.id, 
-      note: "", 
-      reason: selectedUser.reason 
-    };
-    formik.setValues({ rows: updatedRows });
-  }}
-  classNamePrefix="custom-select"
-  styles={{
-    control: (provided, state) => ({
-      ...provided,
-      backgroundColor: theme === 'light' ? '#374151' : '#E5E7EB', 
-      color: theme === 'light' ? '#FFFFFF' : '#000000',
-      height: '48px',
-      borderRadius: '6px',
-      border: 'none',
-      boxShadow: state.isFocused ? '0 0 0 1px #4F46E5' : 'none',
-    }),
-    singleValue: (provided) => ({
-      ...provided,
-      color: theme === 'light' ? '#FFFFFF' : '#000000',
-    }),
-    menu: (provided) => ({
-      ...provided,
-      backgroundColor: theme === 'light' ? '#4B5563' : '#F3F4F6',
-      overflow: 'hidden', 
-    }),
-    menuList: (provided) => ({
-      ...provided,
-      maxHeight: '200px',
-      overflowY: 'auto', 
-      scrollbarWidth: 'none', 
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      backgroundColor: state.isFocused
-        ? theme === 'light'
-          ? '#6B7280'
-          : '#D1D5DB'
-        : 'transparent',
-      color: theme === 'light' ? '#FFFFFF' : '#000000',
-    }),
-  }}
-/>
-
-</td>
+                <tr
+                  key={index}
+                  className={`${
+                    theme === "light"
+                      ? "hover:bg-gray-700"
+                      : "hover:bg-gray-100"
+                  }`}
+                >
+                  <td className="p-2 text-center font-semibold ">
+                    {index + 1}
+                  </td>
+                  <td className="max-w-44">
+                    <Select
+                      options={options}
+                      isSearchable
+                      placeholder="İşçilər..."
+                      menuPlacement="auto"
+                      onChange={(selectedOption) => {
+                        const selectedUser = users.find(
+                          (u) => u.id === selectedOption.value
+                        );
+                        const updatedRows = [...formik.values.rows];
+                        updatedRows[index] = {
+                          id: selectedUser.id,
+                          note: "",
+                          reason: selectedUser.reason,
+                        };
+                        formik.setValues({ rows: updatedRows });
+                      }}
+                      classNamePrefix="custom-select"
+                      styles={{
+                        control: (provided, state) => ({
+                          ...provided,
+                          backgroundColor:
+                            theme === "light" ? "#374151" : "#E5E7EB",
+                          color: theme === "light" ? "#FFFFFF" : "#000000",
+                          height: "48px",
+                          borderRadius: "6px",
+                          border: "none",
+                          boxShadow: state.isFocused
+                            ? "0 0 0 1px #4F46E5"
+                            : "none",
+                        }),
+                        
+                        menuList: (provided) => ({
+                          ...provided,
+                          maxHeight: "200px",
+                          overflowY: "auto",
+                          scrollbarWidth: "none",
+                        }),
+                    
+                      }}
+                    />
+                  </td>
 
                   <td className="p-2 text-center">
                     <input
@@ -189,21 +205,27 @@ export default function UserManagement() {
                       value={row.note}
                       onChange={formik.handleChange}
                       className={`p-1 rounded-md w-full border-none h-12 ${
-                        theme === 'light' ? 'bg-gray-700 text-white' : 'bg-gray-200 text-black'
+                        theme === "light"
+                          ? "bg-gray-700 text-white"
+                          : "bg-gray-200 text-black"
                       }`}
                       placeholder="Xüsusi qeyd"
                     />
                     {formik.errors.rows?.[index]?.note && (
-                      <div className="text-red-500 text-xs mt-1">{formik.errors.rows[index].note}</div>
+                      <div className="text-red-500 text-xs mt-1">
+                        {formik.errors.rows[index].note}
+                      </div>
                     )}
                   </td>
                   <td className="p-2 text-center flex flex-col md:flex-row md:justify-center">
                     <button
-                    key="blur"                  
+                      key="blur"
                       type="button"
                       onClick={() => handleView(row)}
                       className={`py-[2px] md:py-2 px-3 rounded-md mb-1 md:mb-0 md:mr-2 ${
-                        theme === 'dark' ? 'bg-green-500 hover:bg-green-600' : 'bg-green-400 hover:bg-green-500'
+                        theme === "dark"
+                          ? "bg-green-500 hover:bg-green-600"
+                          : "bg-green-400 hover:bg-green-500"
                       }`}
                     >
                       Baxış
@@ -212,7 +234,9 @@ export default function UserManagement() {
                       type="button"
                       onClick={() => handleDeleteRow(index)}
                       className={`py-[2px] md:py-2 px-3 rounded-md ${
-                        theme === 'dark' ? 'bg-red-500 hover:bg-red-600' : 'bg-red-400 hover:bg-red-500'
+                        theme === "dark"
+                          ? "bg-red-500 hover:bg-red-600"
+                          : "bg-red-400 hover:bg-red-500"
                       }`}
                     >
                       Sil
@@ -222,14 +246,13 @@ export default function UserManagement() {
               ))}
             </tbody>
           </table>
-        
         </form>
 
         <UserModal
-        isOpen={isModalOpen}
-        closeModal={() => setIsModalOpen(false)}
-        selectedUser={selectedUser}
-      />
+          isOpen={isModalOpen}
+          closeModal={() => setIsModalOpen(false)}
+          selectedUser={selectedUser}
+        />
       </div>
     </div>
   );
--- src/components/UserModal.jsx
+++ src/components/UserModal.jsx
@@ -1,53 +1,63 @@
 import {
-    Modal,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Button
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
 } from "@nextui-org/react";
 
-
 export default function UM({ isOpen, closeModal, selectedUser }) {
-    return (
-        <Modal
-            isOpen={isOpen}
-            onOpenChange={closeModal}
-           
-            classNames={{
-                backdrop: "bg-black/50 backdrop-blur-sm",
-                base: "rounded-xl shadow-lg",
-                closeButton: "absolute top-3 right-3"
-            }}
-        >
-            <ModalContent className="bg-white min-w-64 max-w-80 sm:max-w-lg relative m-auto  rounded-2xl shadow-xl p-6 border border-gray-200">
-            
-                <ModalHeader className="text-2xl font-bold text-gray-900 border-b pb-2">
-                    İşçi Məlumatı
-                </ModalHeader>
-                <ModalBody className="grid grid-cols-2 gap-4 text-gray-700 pt-4">
-                    {selectedUser ? (
-                        <>
-                            <p><strong>İşçi Nömrəsi:</strong> <span className="text-blue-500 cursor-pointer hover:underline">{selectedUser.employeeNumber}</span></p>
-                            <p><strong>Adı Soyadı:</strong> {selectedUser.name}</p>
-                            <p><strong>Əmrin əsas səbəbi:</strong> {selectedUser.reason}</p>
-                            <p><strong>Qeyd:</strong> {selectedUser.note || "-"}</p>
-                        </>
-                    ) : (
-                        <p className="text-center text-gray-500 col-span-2">Seçilmiş işçi yoxdur.</p>
-                    )}
-                </ModalBody>
-                <ModalFooter className="justify-end border-t pt-3">
-                    <Button 
-                        color="primary" 
-                        variant="solid" 
-                        onPress={closeModal} 
-                        className="bg-blue-600 text-white px-5 py-2 rounded-xl hover:bg-blue-700 transition-all shadow-md"
-                    >
-                        Bağla
-                    </Button>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
-    );
+  return (
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={closeModal}
+      classNames={{
+        backdrop: "bg-black/50 backdrop-blur-sm",
+        base: "rounded-xl shadow-lg",
+        closeButton: "absolute top-3 right-3",
+      }}
+    >
+      <ModalContent className="bg-white min-w-64 max-w-80 sm:max-w-lg relative m-auto  rounded-2xl shadow-xl p-6 border border-gray-200">
+        <ModalHeader className="text-2xl font-bold text-gray-900  pb-2">
+          İşçi Məlumatı
+        </ModalHeader>
+        <ModalBody className="grid grid-cols-2 gap-4 text-gray-700 pt-4">
+          {selectedUser ? (
+            <>
+              <p>
+                <strong>İşçi Nömrəsi:</strong>{" "}
+                <span className="text-blue-500 cursor-pointer hover:underline">
+                  {selectedUser.employeeNumber}
+                </span>
+              </p>
+              <p>
+                <strong>Adı Soyadı:</strong> {selectedUser.name}
+              </p>
+              <p>
+                <strong>Əmrin əsas səbəbi:</strong> {selectedUser.reason}
+              </p>
+              <p>
+                <strong>Qeyd:</strong> {selectedUser.note || "-"}
+              </p>
+            </>
+          ) : (
+            <p className="text-center text-gray-500 col-span-2">
+              Seçilmiş işçi yoxdur.
+            </p>
+          )}
+        </ModalBody>
+        <ModalFooter className="justify-end  pt-3">
+          <Button
+            color="primary"
+            variant="solid"
+            onPress={closeModal}
+            className="bg-blue-600 text-white px-5 py-2 rounded-xl hover:bg-blue-700 transition-all shadow-md"
+          >
+            Bağla
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
